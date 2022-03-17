279e391bfe564d8c78cbc6fa78e8b0d9c9543f8c
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2022-03-17 19:34:22 +0600

    Adding Database

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- Menu section starts -->
+    <div class="menu">
+        <div class="wrapper text-center">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order 51:46(minute)</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu section ends -->
+
+    <!-- Main content starts  -->
+    <div class="main-content">
+        <div class="wrapper">
+        Main content goes here
+        </div>
+    </div>
+    <!-- Main content ends  -->
+
+    <!-- Footer Section Starts  -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2022,All right reserved, Wow Food, Developed by - <a href="#">Ashikur Rahman</a></p>
+        
+        </div>
+    </div>
+    <!-- Footer section ends  -->
+</body>
+</html>
