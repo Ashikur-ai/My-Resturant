74acd76317fc0330b41c7e5c5152fd383e386d00
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2022-04-04 11:19:25 +0600

    adding table and php

--- admin/index.php
+++ admin/index.php
@@ -1,42 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Food Order Website - Home Page</title>
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
-<body>
-    <!-- Menu section starts -->
-    <div class="menu">
-        <div class="wrapper text-center">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Admin</a></li>
-                <li><a href="#">Category</a></li>
-                <li><a href="#">Food</a></li>
-                <li><a href="#">Order 51:46(minute)</a></li>
-            </ul>
-        </div>
-    </div>
-    <!-- Menu section ends -->
-
+<?php include('partials/menu.php');?>
     <!-- Main content starts  -->
     <div class="main-content">
         <div class="wrapper">
-        Main content goes here
+        <h1>DASHBOARD</h1>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+        <div class="clear-fix">
+
+        </div>
+
         </div>
     </div>
     <!-- Main content ends  -->
 
-    <!-- Footer Section Starts  -->
-    <div class="footer">
-        <div class="wrapper">
-            <p class="text-center">2022,All right reserved, Wow Food, Developed by - <a href="#">Ashikur Rahman</a></p>
-        
-        </div>
-    </div>
-    <!-- Footer section ends  -->
-</body>
-</html>
+    <?php include('partials/footer.php');?>
